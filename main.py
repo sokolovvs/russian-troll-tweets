1df8d5a903dcf6e3472b526914f93268878fdc73
Vyacheslav Sokolov <vycheslavsokolov2013@gmail.com>
2021-04-12 13:28:32 +0600

    added writing selected data

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+import csv
 import os
 import re
 import unicodedata
 from _csv import reader
-from typing import Tuple
+from typing import Tuple, List
 
 from nltk.corpus import stopwords
 
@@ -10,15 +11,18 @@ http_pattern = '(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]
 tag_pattern = '(?:(?<=\s)|^)#(\w*[A-Za-z_]+\w*)'
 
 
-def iterate_data(path, filters):
-    total_post = 0
+def iterate_data(path):
+    total_data = 0
+    total_en_ru = 0
+    total_en = 0
+    total_ru = 0
 
     for filename in os.listdir(path):
         if filename.endswith(".csv"):
             with open(path + "/" + filename, 'r') as read_obj:
                 csv_reader = reader(read_obj)
                 for row in csv_reader:
-                    total_post += 1
+                    total_data += 1
 
                     content = row[2]
                     language = row[4]
@@ -27,15 +31,40 @@ def iterate_data(path, filters):
                     if language == 'English' or language == 'Russian':
                         pured_content = clean_stop_words(pured_content)
 
-                    hasTags, hashtags = extract_hashtags(pured_content)
+                        hasTags, hashtags = extract_hashtags(pured_content)
 
-                    if hasTags:
-                        print("Content: {0} \nLanguage: {1} \nPured_content: {2} \nHashtags: {3} \n\n".format(content,
-                                                                                                              language,
-                                                                                                              pured_content,
-                                                                                                              hashtags))
+                        if hasTags:
+                            print_row(content, language, pured_content, hashtags)
 
-    print("Total tweets {0}".format(total_post))
+                            if language == 'English' or language == 'Russian':
+
+                                total_en_ru += 1
+
+                                with open("./data/after/en_ru.csv", mode='a+') as writer:
+                                    csv_writer = csv.writer(writer, delimiter=',', quotechar='"',
+                                                            quoting=csv.QUOTE_MINIMAL)
+
+                                    csv_writer.writerow([pured_content, hashtags, language])
+
+                                if language == 'English':
+                                    total_en += 1
+
+                                    with open("./data/after/en.csv", mode='a+') as writer:
+                                        csv_writer = csv.writer(writer, delimiter=',', quotechar='"',
+                                                                quoting=csv.QUOTE_MINIMAL)
+
+                                        csv_writer.writerow([pured_content, hashtags, language])
+
+                                if language == 'Russian':
+                                    total_ru += 1
+
+                                    with open("./data/after/ru.csv", mode='a+') as writer:
+                                        csv_writer = csv.writer(writer, delimiter=',', quotechar='"',
+                                                                quoting=csv.QUOTE_MINIMAL)
+
+                                        csv_writer.writerow([pured_content, hashtags, language])
+
+    print_total(total_data, total_en_ru, total_en, total_ru)
 
 
 def unicode_to_ascii(s):
@@ -60,11 +89,24 @@ def clean_stop_words(content: str) -> str:
     return " ".join(clean_words)
 
 
-def extract_hashtags(content: str) -> Tuple[bool, list]:
+def extract_hashtags(content: str) -> Tuple[bool, List[str]]:
     tags = re.findall(tag_pattern, content)
 
     return len(tags) != 0, re.findall(tag_pattern, content)
 
 
+def print_row(content: str, language: str, pured_content: str, hashtags: List[str]) -> None:
+    print("Content: {0} \nLanguage: {1} \nPured_content: {2} \nHashtags: {3} \n\n".format(content,
+                                                                                          language,
+                                                                                          pured_content,
+                                                                                          hashtags)
+          )
+
+
+def print_total(total_data, total_en_ru, total_en, total_ru) -> None:
+    print("Total tweets: {0} \nTotal selected EN_RU: {1} \nTotal selected EN: {2} \nTotal selected RU: {3}".format(
+        total_data, total_en_ru, total_en, total_ru))
+
+
 if __name__ == "__main__":
-    iterate_data(path="./data/before", filters={})
+    iterate_data(path="./data/before")
